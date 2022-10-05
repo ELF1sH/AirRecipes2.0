13b1a027e33a87b973a0763f1c38eb054f6e0ffc
Igor <86882588+Ray-Janson@users.noreply.github.com>
2022-10-05 12:51:18 +0700

    Delete unused provider and controller from RoutesWrapper component

--- src/components/routesWrapper/RoutesWrapperController.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import RoutesWrapperView from './RoutesWrapperView';
-
-const RoutesWrapperController = () => (
-  <RoutesWrapperView />
-);
-
-export default RoutesWrapperController;

d3ef36b3c5dcca20da7ffe0508b1168e507fe360
Igor <86882588+Ray-Janson@users.noreply.github.com>
2022-10-11 13:30:55 +0700

    Fixes ARP-0004: Delete unused dependency

--- src/components/defaultComponents/carousel/CarouselView.jsx
+++ src/components/defaultComponents/carousel/CarouselView.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-// import colors from '../../../scssAbstracts/_variables.scss';
-// import styles from './styles/Carousel.module.scss';
 import './styles/carousel.scss';
 
 const CarouselView = ({ images, className }) => {
